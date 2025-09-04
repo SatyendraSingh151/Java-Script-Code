43cdafdda4ae9f5bfe3d20a94913da9d2616783e
Satyendra Singh <151satyendrasingh@gmail.com>
2025-09-04 22:22:06 +0530

    Add files via upload

---
+++ try_catch_block.js
@@ -0,0 +1,20 @@
+// using the try catch and the finally block
+
+console.log("Hello 1 ");
+console.log("Hello 1 ");
+
+
+try{
+    console.log("print a = "+a);
+    
+}catch{
+    console.log("This variable does not exists here. Please try again.");
+    
+}
+// this is the finally block of the 
+finally{
+    console.log("This is the finally block of the code written hare.");
+    
+}
+console.log("Hello 2 ");
+console.log("Hello 2 ");
